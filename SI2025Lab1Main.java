076a9331e66c72c14c242d5c352e070731006b6c
Filip Petrovski <fpetrovski96@gmail.com>
2025-04-13 21:17:31 +0200

    Saving changes

--- SI2025Lab1Main.java
+++ SI2025Lab1Main.java
@@ -91,8 +91,13 @@ class TaskManager {
 
     // 6. Find the highest-priority unfinished task
     public List<Task> getMostUrgentTasks() {
-        // TODO: Implement logic to find most urgent tasks
-        return new ArrayList<>();
+        ArrayList<Task> mostUrgentTasks = new ArrayList<>();
+        for (Task task : tasks) {
+            if (task.getPriority() == Priority.HIGH) {
+                mostUrgentTasks.add(task);
+            }
+        }
+        return mostUrgentTasks;
     }
 
     // 7. Count tasks per category
